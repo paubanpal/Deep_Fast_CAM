9088771a3a512daf72247e10d60eb464b8494d0c
Paula Banares <pau.banpal@gmail.com>
2026-08-06 15:59:53 +0100

    updates

--- unsupervisedMFBD-separate_blocks/model_v6_50_sanity_check_instance_norm.py
+++ unsupervisedMFBD-separate_blocks/model_v6_50_sanity_check_instance_norm.py
@@ -579,7 +579,7 @@ if __name__ == "__main__":
     patience_counter = 0
     best_val_loss = float('inf')
     
-    save_dir = Path("/scratch/paulabp/TFM/model_v6_50_sanity_check_instance_norm")
+    save_dir = Path("/scratch/paulabp/TFM/run_outputs_v6_50_sanity_check_instance_norm")
     save_dir.mkdir(parents=True, exist_ok=True)
     best_model_path = save_dir / "best_model.pt"
 
